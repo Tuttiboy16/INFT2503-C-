7cc5bd2e3820540dcc6e254dab1216dfe4ffcc86
Anders Tellefsen <anders1.tellefsen@gmail.com>
2022-10-20 19:36:46 +0200

    something

--- Oving_7/oving_7/Oppgave_2/main.cpp
+++ Oving_7/oving_7/Oppgave_2/main.cpp
@@ -1,10 +1,8 @@
 #include "set.cpp"
+#include <iostream>
 
 int main() {
   Set set1(vector<int> integersIn = {});
-  //cout << "Set 1: " << set1 << endl;
-  
-  Set set2();
-  cout << "Set 2: " << set2 << endl;
+  cout << "Set 1: " << set1 << endl;
   
 }
--- Oving_7/oving_7/Oppgave_2/set.hpp
+++ Oving_7/oving_7/Oppgave_2/set.hpp
@@ -8,7 +8,6 @@ class Set {
 public:
   vector<int> integers = {5, 23, 6, 12, 7, 8, 2, 1, 6, 3, 65};
   
-  Set();
   Set(vector<int> integersIn);
   Set(vector<int> integersIn, vector<int> integersIn2);
   void operator+(int integerIn);
